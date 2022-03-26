c6dae119d96f0584b1a7a423039ac807c09bb4c4
Thiago Negherbon <thiagonegherbon@outlook.com>
2022-03-26 09:26:20 -0300

    - First commit file addition

---
+++ 1.Namespaces/Api/Program.cs
@@ -0,0 +1,15 @@
+namespace Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ 1.Namespaces/Api/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.OpenApi.Models;
+
+namespace Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddScoped<EventRepository>();
+        services.AddSingleton<EventMapper>();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo {Title = "GloboTicket API", Version = "v1"});
+        });
+        services.AddCors();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseCors(b => b.WithOrigins("https://localhost:5001").AllowAnyHeader().AllowAnyMethod());
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
